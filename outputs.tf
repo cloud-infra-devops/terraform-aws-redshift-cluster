8b6a874bf40bc2141d6c7f6fbbd114f2f65ae08a
Pratik Das <pratik.x.das@gmail.com>
2025-12-13 11:51:21 -0600

    updating redshift cluster output files

--- outputs.tf
+++ outputs.tf
@@ -6,42 +6,38 @@ output "redshift_jdbc" {
   value = module.redshift.cluster_jdbc_url
 }
 
-output "redshift_secret_arn" {
-  value = module.redshift.secret_arn
+output "redshift_cluster_arn" {
+  value = module.redshift.cluster_arn
 }
 
-# output "redshift_cluster_arn" {
-#   value = module.redshift.cluster_arn
-# }
+output "redshift_cluster_identifier" {
+  value = module.redshift.cluster_identifier
+}
 
-# output "redshift_cluster_identifier" {
-#   value = module.redshift.cluster_identifier
-# }
+output "redshift_cluster_endpoint" {
+  value = module.redshift.cluster_endpoint
+}
 
-# output "redshift_cluster_id" {
-#   value = module.redshift.cluster_id
-# }
+output "redshift_cluster_hostname" {
+  value = module.redshift.cluster_hostname
+}
 
-# output "redshift_cluster_endpoint" {
-#   value = module.redshift.cluster_endpoint
-# }
+output "redshift_cluster_dns_name" {
+  value = module.redshift.cluster_dns_name
+}
 
-# output "redshift_cluster_hostname" {
-#   value = module.redshift.cluster_hostname
-# }
+output "redshift_cluster_subnet_group_name" {
+  value = module.redshift.cluster_subnet_group_name
+}
 
-# output "redshift_cluster_dns_name" {
-#   value = module.redshift.cluster_dns_name
-# }
+output "aws_secrets_manager_id" {
+  value = module.redshift.secrets_manager_name
+}
 
-# output "redshift_cluster_subnet_group_name" {
-#   value = module.redshift.cluster_subnet_group_name
-# }
+output "aws_secrets_manager_arn" {
+  value = module.redshift.secrets_manager_arn
+}
 
-# output "aws_secrets_manager_id" {
-#   value = module.redshift.secrets_manager_name
-# }
-
-# output "aws_secrets_manager_arn" {
-#   value = module.redshift.secrets_manager_arn
-# }
+output "post_cluster_secrets_manager_version_id" {
+  value = module.redshift.secret_post_cluster_version_id
+}
--- redshift-cluster/outputs.tf
+++ redshift-cluster/outputs.tf
@@ -1,21 +1,45 @@
+output "cluster_arn" {
+  description = "The Redshift cluster ARN"
+  value       = try(aws_redshift_cluster.this.arn, null)
+}
+
+output "cluster_identifier" {
+  description = "The Redshift cluster identifier"
+  value       = try(aws_redshift_cluster.this.cluster_identifier, null)
+}
+
+output "cluster_subnet_group_name" {
+  description = "Redshift cluster subnet group name"
+  value       = aws_redshift_cluster.this.cluster_subnet_group_name
+}
+
 output "cluster_endpoint" {
-  description = "Redshift cluster endpoint"
-  value       = aws_redshift_cluster.this.endpoint
+  description = "Redshift endpoint address"
+  value       = try(aws_redshift_cluster.this.endpoint, null)
+}
+
+output "cluster_hostname" {
+  description = "The hostname of the Redshift cluster"
+  value = replace(
+    try(aws_redshift_cluster.this.endpoint, ""),
+    format(":%s", try(aws_redshift_cluster.this.port, "")),
+    "",
+  )
+}
+
+output "cluster_dns_name" {
+  description = "The DNS name of the cluster"
+  value       = try(aws_redshift_cluster.this.dns_name, null)
 }
 
 output "cluster_port" {
   description = "Redshift cluster port"
-  value       = aws_redshift_cluster.this.port
+  value       = try(aws_redshift_cluster.this.port, "")
 }
 
 output "cluster_jdbc_url" {
   description = "JDBC connection URL"
-  value       = "jdbc:redshift://${aws_redshift_cluster.this.endpoint}:${aws_redshift_cluster.this.port}/${var.database_name}"
-}
-
-output "secret_arn" {
-  description = "ARN of the Secrets Manager secret containing credentials + endpoint"
-  value       = aws_secretsmanager_secret.this.arn
+  value       = "jdbc:redshift://${aws_redshift_cluster.this.endpoint}:/${var.database_name}"
 }
 
 output "redshift_role_arn" {
@@ -28,7 +52,28 @@ output "s3_log_bucket_name" {
   value       = var.log_destination == "s3" ? (var.create_s3_bucket ? aws_s3_bucket.logs[0].bucket : var.s3_bucket_name) : ""
 }
 
+output "kms_key_id" {
+  description = "KMS key id/arn used for encryption (if created or provided)"
+  value       = aws_kms_key.kms_cmk_key[0].id
+}
+
 output "kms_key_arn" {
   description = "KMS key ARN used for logs/cluster"
   value       = var.kms_key_id != "" ? var.kms_key_id : (length(aws_kms_key.kms_cmk_key) > 0 ? aws_kms_key.kms_cmk_key[0].arn : "")
+}
+
+output "secrets_manager_name" {
+  description = "Name(id) of the Secrets Manager secret that stores master credentials"
+  value       = aws_secretsmanager_secret.this.id
+}
+
+output "secrets_manager_arn" {
+  description = "ARN of the Secrets Manager secret that stores master credentials"
+  value       = aws_secretsmanager_secret.this.arn
+}
+
+output "secret_post_cluster_version_id" {
+  description = "Secrets Manager secret version id that includes endpoint and port (created after cluster)"
+  value       = aws_secretsmanager_secret_version.this.version_id
+  # sensitive   = true
 }
